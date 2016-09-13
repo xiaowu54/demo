006b1f320c15a2b97578f193e501a482aef86d0d
wuq <wuq@startimes.com.cn>
2016-09-13 11:35:52 +0800

    7

--- demo/src/com/HelloWorld.java
+++ demo/src/com/HelloWorld.java
@@ -2,7 +2,7 @@ package com;
 
 public class HelloWorld {
 	public static void main(String[] args) {	
-		System.out.println("123tes6666ttest");
+		System.out.println("123ts6678ttest");
 		System.out.println("testestt");
 		
 	}
