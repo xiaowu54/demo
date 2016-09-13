f857ae28811f6b031c04592486914fd045d210a1
wuq <wuq@startimes.com.cn>
2016-09-13 16:08:33 +0800

    bug

--- demo/src/com/HelloWorld.java
+++ demo/src/com/HelloWorld.java
@@ -2,8 +2,8 @@ package com;
 
 public class HelloWorld {
 	public static void main(String[] args) {	
-		System.out.println("123testtest");
-		System.out.println("test");
-		System.out.println("testMergebug");
+		System.out.println("123tes9898t");
+		//445646
+		//sdfdsf
 	}
 }
