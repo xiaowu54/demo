0f1bac361001ed86124ca910b68eea62e440148e
wuq <wuq@startimes.com.cn>
2016-09-13 13:33:42 +0800

    2

--- demo/src/com/HelloWorld.java
+++ demo/src/com/HelloWorld.java
@@ -2,7 +2,8 @@ package com;
 
 public class HelloWorld {
 	public static void main(String[] args) {	
-		System.out.println("123tes98989ttest");
+
+		System.out.println("123tes9898t");
 		//445646
 	}
 }
