518a3425f42058cffb8febb1f8dd5f9c111ef7ff
xiaowu54 <wuq@startimes.com.cn>
2016-09-13 13:19:31 +0800

    Update HelloWorld.java

--- demo/src/com/HelloWorld.java
+++ demo/src/com/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {	
 		System.out.println("123tes98989ttest");
 		System.out.println("testestt");
+		System.out.println("345435");
 	}
 }
